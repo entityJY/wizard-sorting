9acd60c7126ae9a3f2baf413b310f2fc94c88c79
entityJY <jomathya07@gmail.com>
2025-11-08 00:13:58 -0800

    Basics

--- items/basic_item.gd
+++ items/basic_item.gd
@@ -1,8 +1,9 @@
-extends Sprite2D
+extends Resource
 class_name Item
 
 @export var attributes: Array[String]
 @export var effects: Array[Effect]
+@export var sprite: Resource
 
 func on_sort():
     for effect in effects:
--- levels/level.gd
+++ levels/level.gd
@@ -1,15 +1,32 @@
 extends Node2D
 
-# feel free to change whatever you want in this file, I'm still trying to figure out a good implementation
 
-
-@export var unsorted_stack: Node2D
+@export var unsorted_stack: Array[Item]
 var left_stack: Array[Item]
 var right_stack: Array[Item]
 var discard_stack: Array[Item]
 
-# Contains (3?) variables: left stack, right stack, and unsorted stack (maybe include discard stack?)
-# on left/right, sort the first item in unsorted stack into left/right stack
-#   run on_sort function on sorted item
-# on discard, sort into discard stack
-#   run on_discard function of discarded item
+
+func _process(_delta):
+	if Input.is_action_just_pressed("left"):
+		left_stack.append(unsorted_stack.pop_front())
+	if Input.is_action_just_pressed("right"):
+		right_stack.append(unsorted_stack.pop_front())
+	if Input.is_action_just_pressed("down"):
+		discard_stack.append(unsorted_stack.pop_front())
+	if unsorted_stack.is_empty():
+		calculate_score()
+
+func calculate_score():
+	# needs finishing
+	var left_attributes = {}
+	for item in left_stack:
+		for attribute in item.attributes:
+			var curr = left_attributes.get_or_add(attribute, 0)
+			left_attributes[attribute] = curr + 1
+	
+	var right_attributes = {}
+	for item in right_stack:
+		for attribute in item.attributes:
+			var curr = right_attributes.get_or_add(attribute, 0)
+			right_attributes[attribute] = curr + 1
