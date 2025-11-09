a13cac3ea5e44339feebbf050195e2fe879345aa
entityJY <jomathya07@gmail.com>
2025-11-09 15:12:04 -0800

    Make confusion effect more customizable

--- resources/effects/basic_effect.gd
+++ resources/effects/basic_effect.gd
@@ -1,11 +1,25 @@
 extends Resource
 class_name Effect
 
-func on_discard(_user_effects: UserEffects):
-	pass
 
-func on_sort(_user_effects: UserEffects):
-	pass
+enum effect_target {
+	DISCARD,
+	SORT,
+	ACTIVE,
+}
+@export var target: effect_target
 
-func on_enter_active_sort(_user_effects: UserEffects):
+func on_discard(user_effects: UserEffects):
+	if target == effect_target.DISCARD:
+		effect(user_effects)
+
+func on_sort(user_effects: UserEffects):
+	if target == effect_target.SORT:
+		effect(user_effects)
+
+func on_enter_active_sort(user_effects: UserEffects):
+	if target == effect_target.ACTIVE:
+		effect(user_effects)
+
+func effect(_user_effects: UserEffects):
 	pass
