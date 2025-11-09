367918ff05fa4e2364888ca3efb3f1c63196a307
entityJY <jomathya07@gmail.com>
2025-11-08 17:12:00 -0800

    Change names

--- resources/effects/testing.gd
+++
@@ -1,8 +0,0 @@
-extends Effect
-class_name Testing
-
-func on_discard(_user_effects: UserEffects):
-	print("This was discarded")
-
-func on_sort(_user_effects: UserEffects):
-	print("This was sorted")
