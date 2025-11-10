b713b47d42da293facd934f4533348d5e9b20fbc
hafisaus <superspaceguy0@gmail.com>
2025-11-09 18:14:59 -0800

    fixed levelcompleted ui

--- menus/level_finish_ui.gd
+++ menus/level_finish_ui.gd
@@ -1,5 +1,5 @@
 extends CanvasLayer
 signal level_one_complete(score: int)
 
-func _on_level_1_level_complete(total_score: int) -> void:
+func _on_level_1_level_complete(total_score: int, _completed_all_stacks: bool) -> void:
 	level_one_complete.emit(total_score)
