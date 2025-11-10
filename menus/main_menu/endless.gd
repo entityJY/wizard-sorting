a196b4f271489716b80dea0d58c6043db1736506
hafisaus <superspaceguy0@gmail.com>
2025-11-09 16:38:38 -0800

    added simple transition for main menu

--- menus/main_menu/endless.gd
+++ menus/main_menu/endless.gd
@@ -1,17 +1,17 @@
 extends TextureButton
-#settings
+#level select
 
 
 func _on_pressed() -> void:
 	get_parent().pack_up.emit()
-	await get_tree().create_timer(2.5).timeout
-	get_tree().change_scene_to_file("res://menus/settings/settings.tscn")
 	
+
 func _on_main_menu_pack_up() -> void:
-	await get_tree().create_timer(1.0).timeout
+	await get_tree().create_timer(0.5).timeout
 	for i in range(20):
 		position.y -= i/2
 		await get_tree().create_timer(0.02).timeout
-	for i in range(20):
+	for i in range(40):
 		position.y += i*3
 		await get_tree().create_timer(0.02).timeout
+	
